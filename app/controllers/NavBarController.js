4a8f22338250316e785d186534cfde25d22d189a
Senthilnathan Karuppaiah <senthilsweb@gmail.com>
2015-03-08 17:33:27 +0530

    Change password Modal created

--- app/controllers/NavBarController.js
+++ app/controllers/NavBarController.js
@@ -14,4 +14,25 @@ reachApp.controller('navBarController', function ($rootScope,$scope, $location) 
         $scope.active = "";
         
     });  
+     $scope.items = ['item1', 'item2', 'item3'];
+
+    $scope.open = function (size) {
+
+        var modalInstance = $modal.open({
+            templateUrl: 'myModalContent.html',
+            controller: 'ModalInstanceCtrl',
+            size: size,
+            resolve: {
+                items: function () {
+                    return $scope.items;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
 });
